283f3990ab8b5d3b337ffef3cd2f68fe82dadb6a
SaherZaid <143349286+SaherZaid@users.noreply.github.com>
2024-12-18 23:11:09 +0100

    new

--- BookingRepository/Entities/Booking.cs
+++ BookingRepository/Entities/Booking.cs
@@ -7,7 +7,7 @@ public class Booking
 {
     [BsonRepresentation(BsonType.ObjectId), BsonId]
     public string? Id { get; set; }
-    public int RoomId { get; set; } // the room booked
+    public int RoomId { get; set; } // the room booked.
     public int CustomerId { get; set; } // the customer who booked
     public DateTime CheckInDate { get; set; }
     public DateTime CheckOutDate { get; set; }
